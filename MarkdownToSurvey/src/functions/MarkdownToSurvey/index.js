3ca356fa756984c3fccf14be86f5ebdf069d8693
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-11-17 00:09:07 -0800

    Return 200 explicitly

--- MarkdownToSurvey/src/functions/MarkdownToSurvey/index.js
+++ MarkdownToSurvey/src/functions/MarkdownToSurvey/index.js
@@ -5,10 +5,13 @@ app.http('MarkdownToSurvey', {
     authLevel: 'function',
     handler: async (request, context) => {
         context.log('Markdown to Survey function processed a request.');
+        context.log('Request method:', request.method);
+        context.log('Request headers:', JSON.stringify(request.headers));
 
         const name = request.query.get('name') || await request.text() || 'world';
         
         return {
+            status: 200,
             body: `Hello, ${name}! This is the Markdown to Survey function.`
         };
     }
