12dfd33e2e29a6c9ab1df7e44328732302b305cd
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2024-11-17 00:23:32 -0800

    Set anonymous

--- MarkdownToSurvey/src/functions/MarkdownToSurvey/index.js
+++ MarkdownToSurvey/src/functions/MarkdownToSurvey/index.js
@@ -2,14 +2,14 @@ const { app } = require('@azure/functions');
 
 app.http('MarkdownToSurvey', {
     methods: ['GET', 'POST'],
-    authLevel: 'function',
+    authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log('Markdown to Survey function processed a request.');
         context.log('Request method:', request.method);
         context.log('Request headers:', JSON.stringify(request.headers));
 
         const name = request.query.get('name') || await request.text() || 'world';
-        
+
         return {
             status: 200,
             body: `Hello, ${name}! This is the Markdown to Survey function.`
